3bf2b31086b0c92a9b07580884e19681485eed5d
Ferucci <development.first1@gmail.com>
2025-08-14 14:44:32 +0300

    test script copy

--- scripts/deployEnv.sh
+++ scripts/deployEnv.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 SSH_CONFIG="${1}"
 PROJECT_PATH="${2}"
+
+# Проверка существования .env
+if [ ! -f ".env" ]; then
+  echo "Error: .env file not found in current directory!"
+  exit 1
+fi
+
 scp -v .env "$SSH_CONFIG:${PROJECT_PATH}/current/"
